be0ec353cca5470f58dce23bcff1eb6a457cb5b6
baileyhoare <bailey13404@gmail.com>
2026-08-25 22:12:34 +0930

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -115,4 +115,5 @@ def choose():
     return jsonify({"success": False, "error": "Invalid choice"}), 400
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
