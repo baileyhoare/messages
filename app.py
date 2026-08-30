e30603d855f0641719798adb485c6b2e4e0d6cf4
baileyhoare <bailey13404@gmail.com>
2026-08-30 19:30:42 +0930

    Refactor introductory messages for clarity and tone

--- app.py
+++ app.py
@@ -65,23 +65,21 @@ INTRO_TITLE = "Hey Char"
 
 INTRO_MESSAGE = """
 
-I made a website. It took a while, but I knew I was going to wait until you had left Canada before sending it to you (which I think has happened). You were always great at making fun websites for me. I had to build a dedicated front- and back-end because there are a couple of tricks which I will explain in a minute.
+I made a website. It took a while, but I knew I was going to wait until you had left Canada before sending it to you (which I think has happened). You were always great at making fun websites for me. I had to build a dedicated front- and back-end because there are a couple of tricks which I will explain shortly.
 
 Simply put, I wanted to reach out, with some of my thoughts over the last couple of months. I knew what I wanted to say, but not how to say it, and ended up drafting a couple of versions. I realised that it was because I had no gauge as to how you were doing, and didn't want to upset you or come across in the wrong way, for why I couldn't get the message quite right.
 
-So, I built this website, and you can choose.
+So, I built this website, and you can choose. I understand choosing is difficult, especially for you. Do not worry too much, the messages all say more or less the same thing, but each one has a different purpose and emphasis.
 
-There are three messages. They all come from the same place, and they all contain some of the same things I have been thinking about, but each one has a different purpose and emphasis.
+Message 1 is the most personal. It is mainly about how I feel now: missing you, what I remember about us, what I regret, and the things I have found difficult since we stopped speaking. It is probably the most vulnerable of the three, and you should choose it if you have been sad, and have missed what we had.
 
-Message 1 is the most personal. It is mainly about how I feel now: missing you, what I remember about us, what I regret, and the things I have found difficult since we stopped speaking. It is probably the most vulnerable of the three.
+Message 2 is more reflective. It is less about simply telling you that I miss you, and more about trying to understand what happened between us. It talks about resentment, our future, my mistakes, the effect of the distance, and what I have come to understand after having some time to think. This contains the most depth, and also mentions what the next months look like for me. Choose this for a down-the-middle, in depth review.
 
-Message 2 is more reflective. It is less about simply telling you that I miss you, and more about trying to understand what happened between us. It talks about resentment, our future, my mistakes, the effect of the distance, and what I have come to understand after having some time to think.
-
-Message 3 is the closure version. It contains many of the same feelings as Message 1, but without trying to leave the door open in the same way. It is essentially the things I would want you to know if we never spoke again: what I am sorry for, what I appreciate about what we had, and what I hope for you going forward.
+Message 3 is the closure version. It contains many of the same feelings as Message 1, essentially the things I would want you to know if we never spoke again: what I am sorry for, what I appreciate about what we had, and what I hope for you going forward. Choose this if you're ready to move on (or have moved on!).
 
 None of the three is intended to be the "right" choice. They are just three different ways of saying the things I have been carrying around, depending on what you feel most comfortable reading.
 
-Once you choose one, that is the only message you will be able to read. The other two will be permanently locked by the server, so there genuinely isn't a way for me to give you access to them afterwards. So, be careful.
+Once you choose one, that is the only message you will be able to read. The other two will be permanently locked, so there genuinely isn't a way for me to give you access to them afterwards. So, be careful.
 
 I thought about adding some fun easter eggs, but didn't want to overdo it. A nice colour scheme of sage green and sunset orange, along with the security question (well done on passing) should do it.
 
@@ -400,7 +398,7 @@ HTML_TEMPLATE = """
 
     <meta charset="UTF-8">
 
-    <title>Choose Wisely</title>
+    <title>My Thoughts</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
